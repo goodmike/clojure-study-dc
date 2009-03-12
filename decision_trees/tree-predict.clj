1303b1b54d5ca8c8799de9cb04ce56814af24a18
Michael Harrison <mh@michaelharrison.ws>
2009-03-12 16:35:38 -0400

    Added unique-counts function

--- decision_trees/tree-predict.clj
+++ decision_trees/tree-predict.clj
@@ -23,7 +23,8 @@
 
 ;; Classification And Regression Trees algorithm
 (defn divide-set
-  "divides data rows into two sets based on the data in a specific column"
+  "divides data rows into two sets based on the data in a specific column:
+   [row][col], col, test-value => {:true-set :false-set}"
   [rows col value]
   (let [test-op
 	(if (.isInstance java.lang.Number value)
@@ -32,3 +33,9 @@
 	[true-set false-set] (separate #(test-op (% col) value) rows)]
     {:true-set true-set :false-set false-set}))
 
+(defn unique-counts
+  "count occurences of each result in a set of rows:
+  [row][col], result-col => {[result occurences]}"
+  [rows, result-col]
+  (reduce (fn [accum row] (assoc accum (row result-col) (inc (accum result-col)))) {} rows))
+    
